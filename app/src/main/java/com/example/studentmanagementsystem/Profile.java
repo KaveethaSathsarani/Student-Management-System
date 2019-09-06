558bf86aa8ed4c1f95b0c85db1f3c2543679f427
VisithaRukshan <vishusptfy@gmail.com>
2019-09-06 16:51:57 +0530

    Visitha Commited successfully

---
+++ app/src/main/java/com/example/studentmanagementsystem/Profile.java
@@ -0,0 +1,25 @@
+package com.example.studentmanagementsystem;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Profile extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile,null);
+    }
+}
+
---
+++ app/src/main/java/com/example/studentmanagementsystem/mainClass.java
@@ -0,0 +1,67 @@
+package com.example.studentmanagementsystem;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class mainClass extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new Profile());
+    }
+
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId())
+        {
+            case R.id.navigation_profile:
+                fragment = new Profile();
+                break;
+
+            case R.id.navigation_exam:
+                fragment = new Exam();
+                break;
+
+            case R.id.navigation_papers:
+                fragment = new Papers();
+                break;
+
+            case R.id.navigation_timeTable:
+                fragment = new TimeTable();
+                break;
+
+            case R.id.notification_button:
+                fragment = new Notifications();
+                break;
+
+        }
+        return loadFragment(fragment);
+    }
+}
