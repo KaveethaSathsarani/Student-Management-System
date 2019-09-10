17c477b8415b62d5fa23b5bfedbfad6b4b4d692d
KaveethaSathsarani <kaveethasathsarani2345@gmail.com>
2019-09-10 17:24:25 +0530

    kaveetha commited

--- app/src/main/java/com/example/studentmanagementsystem/Papers.java
+++ app/src/main/java/com/example/studentmanagementsystem/Papers.java
@@ -4,18 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Objects;
 
 
 public class Papers extends Fragment {
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_papers,null);
-    }
+        //return inflater.inflate(R.layout.fragment_papers,null);
+
+        View v =inflater.inflate(R.layout.fragment_papers, container, false);
+
+        return v;
+
+
+        }
+
+
 }
 
--- app/src/main/java/com/example/studentmanagementsystem/mainClass.java
+++ app/src/main/java/com/example/studentmanagementsystem/mainClass.java
@@ -1,5 +1,7 @@
 package com.example.studentmanagementsystem;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -8,7 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class mainClass extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -64,4 +68,22 @@ public class mainClass extends AppCompatActivity implements BottomNavigationView
         }
         return loadFragment(fragment);
     }
-}
+
+    public void callNotices(View view) {
+
+
+        Fragment fragment1;
+
+        if (view == findViewById(R.id.notification_button)) {
+            Log.i("Info","notification button clicked");
+            fragment1 = new Notifications();
+            //FragmentManager fm1 = getFragmentManager();
+            //FragmentTransaction ft = fm1.beginTransaction();
+            //ft.replace(R.id.fragment_container,fragment1);
+            //ft.commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment1).commit();
+
+        }
+    }
+
+    }
